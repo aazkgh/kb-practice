eb5a2bc27669f48bf1006c668e42481f1b46e5cc
aazkgh <gg25hp17@naver.com>
2025-03-26 17:35:08 +0900

    feaet: 메뉴 선택 구현

---
+++ vue-proj/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <section>
+    <MenuList :menus=menuList @select-drink="handleSelectDrink" />
+    <div v-if="selectedDrink" class="selected">
+      <h4>선택한 음료: {{ selectedDrink }}</h4>
+    </div>
+  </section>
+</template>
+<script lang="ts">
+import MenuList from './components/MenuList.vue';
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      menuList : [
+        { name: "아메리카노", price: 4000 },
+        { name: "카페라떼", price: 4500 },
+        { name: "바닐라라떼", price: 4800 },
+        { name: "콜드브루", price: 5000 }
+      ],
+      selectedDrink: '',
+    }
+  },
+  components: {
+    MenuList
+  },
+  methods: {
+    handleSelectDrink(drinkName : string) {
+      this.selectedDrink = drinkName;
+    },
+  },
+}
+</script>
+<style scoped>
+  body {
+    margin: 0;
+    background-color: white;
+    color: black;
+  }
+  .selected {
+    margin-top: 20px;
+    padding: 10px;
+    border: 1px solid #ddd;
+    background-color: #f7f7f7;
+  }
+</style>
---
+++ vue-proj/src/components/MenuList.vue
@@ -0,0 +1,21 @@
+<template>
+  <main>
+    <h2>메뉴 선택</h2>
+    <button v-for="menu in menus" type="button" 
+      class="menuTag"  @click="$emit('select-drink', menu.name)">
+      {{ menu.name }} ({{ menu.price }}원)
+    </button>
+  </main>
+</template>
+<script setup lang="ts">
+defineProps<{
+  menus: Array<{ name: string; price: number }>
+}>()
+</script>
+<style scoped>
+.menuTag {
+  margin: 5px;
+  padding: 10px;
+  cursor: pointer;
+}
+</style>

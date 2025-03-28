bb8dbb25e0284109ee126e5eba360af4e023596c
aazkgh <gg25hp17@naver.com>
2025-03-28 16:45:18 +0900

    feaet:랜덤 대사 출력기

---
+++ vue-proj/src/acting-club-components/LineReadyButton.vue
@@ -0,0 +1,44 @@
+<template>
+    <section class="btnContainer">
+        <button type="button" id="readyBtn" @click="emitReady">
+            📽 대사 준비 완료
+        </button>
+        <br />
+        버튼을 누른 횟수: {{ count }}회 
+    </section>
+</template>
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const count = ref(0);
+
+const readyEmits = defineEmits<{
+    (e: 'ready') : void
+}>();
+
+const emitReady = () => {
+    count.value++
+    readyEmits('ready');
+};
+</script>
+<style>
+    .btnContainer {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+    #readyBtn {
+        height: 5rem;
+        width: 12rem;
+
+        background-color: #ff7675;
+        color: white;
+        border-radius: 12px;
+        text-align: center;
+        font-size: large;
+
+        &:hover {
+            background-color: #d63031;
+        }
+    }
+</style>

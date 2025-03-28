a9ecb828ea634d9fe5ea503083d776bdd8ce78b2
aazkgh <gg25hp17@naver.com>
2025-03-28 10:32:54 +0900

    feat: 음료 옵션 선택 구현

--- vue-proj/src/App.vue
+++ vue-proj/src/App.vue
@@ -4,11 +4,18 @@
     <div v-if="selectedDrink" class="selected">
       <h4>선택한 음료: {{ selectedDrink }}</h4>
     </div>
-    <DrinkOptions :selectedDrink />
+    <DrinkOptions v-if="selectedDrink" :selectedDrink @options-changed="handleOptionChange"/>
+    <div v-if="selectedOptions.size && selectedOptions.ice" class="selected">
+      <h4>선택한 옵션:</h4>
+        <br /> 사이즈: {{ selectedOptions.size }}
+        <br /> 추가샷: {{ selectedOptions.extraShot ? '있음' : '없음' }}
+        <br /> 얼음 양: {{ selectedOptions.ice }}
+    </div>
+
   </main>
 </template>
 <script lang="ts">
-import DrinkOptions from './components/DrinkOptions.vue';
+import DrinkOptions, { type DrinkOptionType } from './components/DrinkOptions.vue';
 import MenuList from './components/MenuList.vue';
 
 export default {
@@ -22,15 +29,20 @@ export default {
         { name: "콜드브루", price: 5000 }
       ],
       selectedDrink: '',
+      selectedOptions: <DrinkOptionType>({})
     }
   },
   components: {
-    MenuList
+    MenuList,
+    DrinkOptions
   },
   methods: {
     handleSelectDrink(drinkName : string) {
       this.selectedDrink = drinkName;
     },
+    handleOptionChange(options: DrinkOptionType){
+      this.selectedOptions = options;
+    }
   },
 }
 </script>
@@ -41,8 +53,8 @@ export default {
     color: black;
   }
   .selected {
-    margin-top: 20px;
-    padding: 10px;
+    margin-top: 2rem;
+    padding: 1rem;
     border: 1px solid #ddd;
     background-color: #f7f7f7;
   }
--- vue-proj/src/components/DrinkOptions.vue
+++ vue-proj/src/components/DrinkOptions.vue
@@ -1,18 +1,65 @@
-<template lang="ts">
-    <div>
-        
-    </div>
+<template>
+    <section id="chooseOption">
+        <h2>{{ selectedDrink }} 옵션 선택</h2>
+        <div>
+            사이즈: 
+            <input type="radio" name="size" value="Small" v-model="drinkOptions.size" @change="emitOptions">Small
+            <input type="radio" name="size" value="Medium" v-model="drinkOptions.size" @change="emitOptions" checked>Medium
+            <input type="radio" name="size" value="Large" v-model="drinkOptions.size" @change="emitOptions">Large
+        </div>
+        <div>
+            <input type="checkbox" v-model="drinkOptions.extraShot" @change="emitOptions">샷 추가
+        </div>
+        <div>
+            얼음 양: 
+            <select v-model="drinkOptions.ice" @change="emitOptions">
+                <option value="less">적게</option>
+                <option value="normal" selected>보통</option>
+                <option value="more">많이</option>
+            </select>
+        </div>
+    </section>
 </template>
-<script lang="ts">
-export default {
-    name: 'DrinkOptions',
-    data() {
-        return {
-            
-        }
-    },
+<script setup lang="ts">
+import { reactive } from 'vue';
+
+defineProps<{
+    selectedDrink: string
+}>()
+
+export interface DrinkOptionType {
+    size: string;
+    extraShot: boolean;
+    ice: string;
 }
+
+const drinkOptions = reactive<DrinkOptionType>({
+    size: 'Medium',
+    extraShot: false,
+    ice: 'normal'
+});
+
+//emit 정의
+const optionEmits = defineEmits<{
+    (e: 'options-changed', options: DrinkOptionType) : void
+}>();
+
+//event 감지
+const emitOptions = () => {
+    optionEmits('options-changed', drinkOptions);
+};
+
 </script>
-<style>
-    
+<style scoped>
+    #chooseOption {
+        padding: 0.4rem 1.2rem 1.2rem;
+        margin: 2rem 0;
+
+        display: flex;
+        flex-direction: column;
+        gap: 1.2rem;
+
+        background-color: lavenderblush;
+
+    }
 </style>

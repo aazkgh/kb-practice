52e6ce24832c6a3b706634bc5d38b497a08ac0c2
aazkgh <ghgh20@ewhain.net>
2025-04-24 16:24:15 +0900

    music player: play func

--- JAVA/src/MyGroove/Main.java
+++ JAVA/src/MyGroove/Main.java
@@ -2,10 +2,18 @@ package MyGroove;
 
 public class Main {
     public static void main(String[] args) {
-        PlaybackLogger logger = new PlaybackLogger();
+        MusicPlayer player = new MusicPlayer();
 
-        logger.log("사건의 지평선", "윤하");
-        logger.log("오늘만 I LOVE YOU", "BOYNEXTDOOR");
-        logger.log("Ditto", "NewJeans");
+        player.play("Ditto", false, new MusicPlayer.PlayResultListener() {
+            @Override
+            public void onSuccess(String title) {
+                System.out.println("✅ 재생 성공: " + title);
+            }
+
+            @Override
+            public void onError(String title, int errorCode) {
+                System.out.println("⁉ 재생 실패: " + title + " - 오류 코드:" + errorCode);
+            }
+        });
     }
 }

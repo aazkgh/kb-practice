e604ced3296540645e33c0e2f689a67e3090416d
aazkgh <gahyungko@gmail.com>
2025-05-29 16:26:55 +0900

    metamong: filter choice

--- JAVA/src/PopUp/Main.java
+++ JAVA/src/PopUp/Main.java
@@ -1,18 +1,39 @@
 package PopUp;
 
-import PopUp.framework.Ticket;
-import PopUp.framework.TicketFactory;
-import PopUp.metamong.MetamongTicketFactory;
+import PopUp.framework.Goods;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
 
 public class Main {
     public static void main(String[] args) {
-        TicketFactory factory = new MetamongTicketFactory();
-        Ticket jiwoo = factory.create("지우");
-        Ticket isule = factory.create("이슬");
+        List<Goods> goods = new ArrayList<Goods>();
 
-        System.out.println();
+        goods.add(new Goods("메타몽 쿠션", "분홍", false));
+        goods.add(new Goods("메타몽 노트북 파우치", "보라", true));
+        goods.add(new Goods("메타몽 인형", "분홍", false));
+        goods.add(new Goods("메타몽 가방", "회색", true));
+        goods.add(new Goods("메타몽 키링", "파랑", false));
 
-        jiwoo.enter();
-        isule.enter();
+        Predicate<Goods> jiwooFilter = g -> g.getColor().equals("분홍");
+        Predicate<Goods> iseulFilter = g -> g.isUseful();
+
+        printSelectedGoods("지우", goods, jiwooFilter);
+        printSelectedGoods("이슬이", goods, iseulFilter);
+
+    }
+
+    public static void printSelectedGoods(String user, List<Goods> list ,Predicate<Goods> filter) {
+        System.out.println(user + "의 선택");
+
+        List<Goods> selectedGoods = new ArrayList<Goods>();
+        list.forEach(g -> {
+            if (filter.test(g)) {
+                selectedGoods.add(g);
+            }
+        });
+
+        selectedGoods.forEach(g -> System.out.println(g.getName()));
     }
 }
---
+++ JAVA/src/PopUp/framework/Goods.java
@@ -0,0 +1,24 @@
+package PopUp.framework;
+
+public class Goods {
+    private String name;
+    private String color;
+    private boolean useful;
+
+    public Goods(String name, String color, boolean useful) {
+        this.name = name;
+        this.color = color;
+        this.useful = useful;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+    public boolean isUseful() {
+        return useful;
+    }
+}
